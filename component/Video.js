f4c1035a83580775d6a591d7f3a69b1eb653d81b
Marczeli <aleczas1@onet.pl>
2020-10-25 15:01:27 +0100

    ad

--- component/Video.js
+++ component/Video.js
@@ -2,7 +2,7 @@ import React, { Component, useState, useEffect, useRef } from 'react';
 import { Player, ControlBar } from 'video-react';
 import { Left } from '../svg/Svg';
 
-//https://video-react.js.org/components/player/
+//https://video-react.js.org/
 
 export default class Video extends Component {
   state = {
@@ -12,7 +12,7 @@ export default class Video extends Component {
 
   componentDidMount() {
     // subscribe state change
-    this.player.subscribeToStateChange(this.handleStateChange.bind(this));
+    // this.player.subscribeToStateChange(this.handleStateChange.bind(this));
 
     setTimeout(() => {
       this.setState({ showVideo: true });
@@ -20,105 +20,66 @@ export default class Video extends Component {
 
     this.setState({ innerWidth: window.innerWidth });
 
-    setTimeout(() => {
-      this.play();
-    }, 1000);
+    // setTimeout(() => {
+    //   this.play();
+    // }, 1000);
   }
 
-  handleStateChange = (state) => {
-    // copy player state to this component's state
-    this.setState({
-      player: state,
-    });
-  };
+  // handleStateChange = (state) => {
+  //   // copy player state to this component's state
+  //   this.setState({
+  //     player: state,
+  //   });
+  // };
 
-  play = () => {
-    this.player.play();
-  };
+  // play = () => {
+  //   this.player.play();
+  // };
 
-  pause = () => {
-    this.player.pause();
-  };
+  // pause = () => {
+  //   this.player.pause();
+  // };
 
-  hideOrOpen = () => {
-    if (this.state.showVideo === true) {
-      this.setState({ showVideo: false });
-      this.pause();
-      // set to 0sec
-      this.player.seek(0);
-    } else {
-      this.setState({ showVideo: true });
-      setTimeout(() => {
-        this.play();
-      }, 1000);
-    }
-  };
+  // hideOrOpen = () => {
+  //   if (this.state.showVideo === true) {
+  //     this.setState({ showVideo: false });
+  //     this.pause();
+  //     // set to 0sec
+  //     this.player.seek(0);
+  //   } else {
+  //     this.setState({ showVideo: true });
+  //     setTimeout(() => {
+  //       this.play();
+  //     }, 1000);
+  //   }
+  // };
 
   render() {
     return (
       <div className={`interactive ${this.state.showVideo ? 'open' : ''}`}>
-        <span
-          className={`toggleVideo`}
-          onClick={() => this.hideOrOpen()}
-        >
+        <span className={`toggleVideo`} onClick={() => this.hideOrOpen()}>
           <Left />
         </span>
         <div>
           {this.state.innerWidth < 425 && (
-            <Player
-              ref={(player) => {
-                this.player = player;
-              }}
-              poster='./teacher.png'
-              width={200}
-              height={113}
-              fluid={false}
-            >
-              <source src='./video.mp4' />
-              <ControlBar autoHide={false} />
-            </Player>
+            <video width='200' height='113' controls>
+              <source src='./video.mp4' type='video/mp4' />
+            </video>
           )}
           {this.state.innerWidth >= 425 && this.state.innerWidth < 768 && (
-            <Player
-              ref={(player) => {
-                this.player = player;
-              }}
-              poster='./teacher.png'
-              width={250}
-              height={142}
-              fluid={false}
-            >
-              <source src='./video.mp4' />
-              <ControlBar autoHide={false} />
-            </Player>
+            <video width='250' height='142' controls>
+              <source src='./video.mp4' type='video/mp4' />
+            </video>
           )}
           {this.state.innerWidth >= 768 && this.state.innerWidth < 1024 && (
-            <Player
-              ref={(player) => {
-                this.player = player;
-              }}
-              poster='./teacher.png'
-              width={280}
-              height={159}
-              fluid={false}
-            >
-              <source src='./video.mp4' />
-              <ControlBar autoHide={false} />
-            </Player>
+            <video width='280' height='159' controls>
+              <source src='./video.mp4' type='video/mp4' />
+            </video>
           )}
           {this.state.innerWidth >= 1024 && (
-            <Player
-              ref={(player) => {
-                this.player = player;
-              }}
-              poster='./teacher.png'
-              width={300}
-              height={170}
-              fluid={false}
-            >
-              <source src='./video.mp4' />
-              <ControlBar autoHide={false} />
-            </Player>
+            <video width='300' height='170' controls>
+              <source src='./video.mp4' type='video/mp4' />
+            </video>
           )}
         </div>
       </div>
